de284c437c13c5cbe79c385ea452cdb301788746
Cronan321 <cronan02@outlook.com>
2026-06-29 05:14:14 -0400

    Revert to charcoal/orange dark theme, remove logo.svg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 import './Header.css';
 
 function Header() {
